e546f7522c8a4f8181e56156f9ffe051f963d40b
Kelly Leach <101753828+Keltonlea@users.noreply.github.com>
2022-07-26 16:44:30 -0700

    dont need date

--- utils/helpers.js
+++ utils/helpers.js
@@ -1,9 +1,4 @@
-//-- Formatting Date
-const format_date = date => {
-    return `${new Date(date).getMonth() + 1}/${new Date(date).getDate()}/${new Date(
-      date
-    ).getFullYear()}`;
-  };
+
   
   // comparison operators
   const eq = (var1, var2) =>  { console.log(var1,var2);return var1 === var2 };
@@ -38,7 +33,6 @@ const format_date = date => {
   
   //-- EXPORTS
   module.exports = {
-    format_date,
     eq, ne, lt, gt, lte, gte, 
     format_prof
   }
